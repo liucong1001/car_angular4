4941a0999329805197830262cbab0b2426d3f541
chenlh <chenlh@34825b8f-b11c-4bce-a188-27078bf7e737>
2017-12-08 12:01:41 +0000

    add bussiness data and model and pre judication recording-continue detail and list is ok under parent component

--- carexcn/trunk/web/src/app/pages/bussiness/prejudication/recording-continue/car-detail/car-detail.component.ts
+++ carexcn/trunk/web/src/app/pages/bussiness/prejudication/recording-continue/car-detail/car-detail.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'ngx-car-detail',
   templateUrl: './car-detail.component.html',
-  styleUrls: ['./car-detail.component.scss']
+  styleUrls: ['./car-detail.component.scss'],
 })
 export class CarDetailComponent implements OnInit {
-
-  constructor() { }
+  @Input() car;
+  constructor(private _router: Router) { }
 
   ngOnInit() {
   }
-
+  readCard() {
+  }
+  onSubmit() {
+    this._router.navigateByUrl('/pages/bussiness/prejudication/recording-last');
+  }
+  reBack() {
+    this._router.navigateByUrl('/pages/bussiness/prejudication');
+  }
 }
--- carexcn/trunk/web/src/app/pages/bussiness/prejudication/recording-continue/recording-continue.component.ts
+++ carexcn/trunk/web/src/app/pages/bussiness/prejudication/recording-continue/recording-continue.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-// import {CarService} from './car.service';
+import {Component, OnInit} from '@angular/core';
+import {MessageService} from '../../../../@core/utils/message.service';
+import {CarModel} from '../../../../@core/model/bussiness/car.model';
+import {CarService} from '../../../../@core/data/bussiness/car.service';
 
 @Component({
   selector: 'ngx-recording-continue',
   templateUrl: './recording-continue.component.html',
-  styleUrls: ['./recording-continue.component.scss']
+  styleUrls: ['./recording-continue.component.scss'],
+  providers: [CarService],
 })
 
 export class RecordingContinueComponent implements OnInit {
-  // car;
-  // constructor(carservice:CarService) {
-  //   this.car = carservice.getCars();
-  // }
-
-  ngOnInit() {
+  public carData: CarModel;
+  public carsData: CarModel[];
+  constructor(private message: MessageService, private carService: CarService) {
+    this.carService.getCar('1').then((res) => this.carData = res as CarModel);
+    this.carService.getCars('1').then((res) => this.carsData = res as CarModel[]);
   }
-  // selectedCar(car:any) {
-  //   // console.log(car);
-  //   this.car.lsnum = car.lsnum;
-  // }
+  ngOnInit(): void {
+  }
+  onChangeSelectingCar(car: CarModel): void {
+    this.carData = car;
+    this.message.info(this.carData.lsnum, '当前车辆');
+  }
 }
