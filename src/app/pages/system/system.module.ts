aaff6afcc138f06f4884c09438d80adf80f49dfd
chenlh <chenlh@34825b8f-b11c-4bce-a188-27078bf7e737>
2017-12-12 04:08:26 +0000

    update for ui module

--- src/app/pages/system/system.module.ts
+++ src/app/pages/system/system.module.ts
@@ -7,19 +7,19 @@ import {NgModule} from '@angular/core';
 import {ThemeModule} from '../../@theme/theme.module';
 import {routedComponents, SystemRoutingModule} from './system-routing.module';
 import {HttpModule} from '@angular/http';
-import {UiTableModule} from '../../@core/ui/table/table.module';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {CodeEditComponent} from './code/code-edit.component';
 import {CodeitemEditComponent} from './code/codeitem-edit.component';
 import { TransferCityitemEditComponent } from './transfer-city/transferCityitem-edit.component';
+import {UiModule} from '../../@core/ui/ui.module';
 
 const components = [
   CodeitemEditComponent, TransferCityitemEditComponent,
 ];
 
 @NgModule({
-  imports: [ThemeModule, SystemRoutingModule,  HttpModule, UiTableModule, CommonModule, RouterModule ],
+  imports: [ThemeModule, SystemRoutingModule,  HttpModule, UiModule, CommonModule, RouterModule ],
   declarations: [...routedComponents, ...components],
   providers: [],
 })
