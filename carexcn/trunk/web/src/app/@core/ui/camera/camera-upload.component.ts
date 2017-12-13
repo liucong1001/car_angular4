0592349e285d7c78da1670b7d6ff5143a84e53c8
chenlh <chenlh@34825b8f-b11c-4bce-a188-27078bf7e737>
2017-12-13 11:58:53 +0000

    [WARN:exec command `npm i` after update ]: upgrade camera support upload local image

---
+++ carexcn/trunk/web/src/app/@core/ui/camera/camera-upload.component.ts
@@ -0,0 +1,55 @@
+import {Component, EventEmitter, Input, isDevMode, OnInit, Output, ViewEncapsulation} from '@angular/core';
+import {MessageService} from '../../utils/message.service';
+import {FileUploadModule} from 'primeng/primeng';
+
+@Component({
+  selector: 'ngx-ys-camera-upload',
+  templateUrl: './camera-upload.component.html',
+  styles: [`
+    p-fileupload input {
+      position: absolute;
+      top: 3px;
+      right: 5px;
+      margin: 0;
+      opacity: 0;
+      height: 30px;
+      width: 30px;
+      float: right;
+      text-align: right;
+      filter: alpha(opacity=0);
+      cursor: pointer;
+    }
+    `],
+  providers: [FileUploadModule],
+  encapsulation: ViewEncapsulation.None,
+})
+export class CameraUploadComponent implements OnInit {
+  @Output('_new_img_url') private _new_img_url = new EventEmitter();
+  public upload_url: string;
+
+  /**
+   * 构造函数
+   * 目前暂时只适配了开发环境[自己搭建的调试环境]
+   * TODO: 需要在后端完成时进一步适配线上环境，对接后端程序
+   * @param {MessageService} message
+   * @param {FileUploadModule} upld
+   */
+  constructor(private message: MessageService, private upld: FileUploadModule) {
+    const self = this;
+    if (isDevMode()) {
+      this.upload_url = 'http://dongshenghuo.com/test.php';
+    } else {
+      this.upload_url = location.protocol + '//' + location.host + '/files/upload';
+    }
+  }
+  ngOnInit() {
+  }
+  uploadProgress($event) {
+    // this.message.info('上传进度', $event.progress + '%');
+  }
+  uploadComplete($event) {
+    const res = JSON.parse($event.xhr.responseText);
+    this._new_img_url.emit(res.file[0]);
+    this.message.info('上传', '上传完成');
+  }
+}
