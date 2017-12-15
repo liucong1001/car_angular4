ab0e4e302389f32675f8686d51b38d51166910f4
chenlh <chenlh@34825b8f-b11c-4bce-a188-27078bf7e737>
2017-12-15 12:27:32 +0000

    update recording continue forword backend api update page title

--- src/app/pages/bussiness/prejudication/recording-continue/car-detail/car-detail.component.ts
+++ src/app/pages/bussiness/prejudication/recording-continue/car-detail/car-detail.component.ts
@@ -1,13 +1,28 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {DeviceService} from '../../../../../@core/device/device.service';
 
 @Component({
   selector: 'ngx-car-detail',
   templateUrl: './car-detail.component.html',
   styleUrls: ['./car-detail.component.scss'],
+  providers: [DeviceService],
 })
 export class CarDetailComponent implements OnInit {
   @Input() car;
+  photos: any[] = [{
+    title: '行驶证正本',
+    source: 'assets/images/camera1.jpg',
+  }, {
+    title: '行驶证副本',
+    source: 'assets/images/camera2.jpg',
+  }, {
+    title: '登记证书首页',
+    source: 'assets/images/camera3.jpg',
+  }, {
+    title: '登记证书末页',
+    source: 'assets/images/camera4.jpg',
+  }];
   constructor(private _router: Router) { }
 
   ngOnInit() {
