22af3abe7729e3590a4026f3a93266c0b0a970d2
chenlh <chenlh@34825b8f-b11c-4bce-a188-27078bf7e737>
2017-12-15 12:27:32 +0000

    update recording continue forword backend api update page title

--- carexcn/trunk/web/src/app/pages/bussiness/prejudication/recording-continue/recording-continue.component.ts
+++ carexcn/trunk/web/src/app/pages/bussiness/prejudication/recording-continue/recording-continue.component.ts
@@ -7,6 +7,9 @@ import {CarService} from '../../../../@core/data/bussiness/car.service';
  * 预审录入 继续录入/批量录入 --接口与页面的交互逻辑
  * 1、请求接口(通过批次号)，或者要求接收的参数包含：商户编号，联系人姓名，手机号
  * 2、用户点击不同的车辆则调用后台接口获取不同车辆的信息
+ * 3、读取行驶证时通过云接口调取行驶证的内容填充进表单
+ * 4、允许用户修改部分表单内容
+ * 5、获取表单最终内容并提交到后台接口并跳转到提交成功按钮。
  */
 @Component({
   selector: 'ngx-recording-continue',
@@ -15,6 +18,7 @@ import {CarService} from '../../../../@core/data/bussiness/car.service';
   providers: [CarService],
 })
 export class RecordingContinueComponent implements OnInit {
+  public test='';
   public carData: CarModel;
   public carsData: CarModel[];
   constructor(private message: MessageService, private carService: CarService) {
