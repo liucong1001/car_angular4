b875f8ba6789990e5eb0443166be6c0627952ff0
chenlh <chenlh@34825b8f-b11c-4bce-a188-27078bf7e737>
2017-12-21 10:26:18 +0000

    support json to httpparam

--- src/app/@core/utils/rest.service.ts
+++ src/app/@core/utils/rest.service.ts
@@ -1,9 +1,16 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 /**
  * RestService 项目接口请求核心服务
  */
 @Injectable()
 export class RestService extends HttpClient {
+  public json2httpparam(json_data: object): HttpParams {
+    let httpParams = new HttpParams();
+    Object.keys(json_data).forEach(function (key) {
+      httpParams = httpParams.append(key, json_data[key]);
+    });
+    return httpParams;
+  }
 }
