993ee6fe82f8cfb12a0d5c3153e96a0825c5210f
chenlh <chenlh@34825b8f-b11c-4bce-a188-27078bf7e737>
2017-12-23 05:50:55 +0000

    add codeitem droplist and droplist  ui component and example is ok

--- carexcn/trunk/web/src/app/@core/ui/codeitem-droplist/codeitem-droplist.component.ts
+++ carexcn/trunk/web/src/app/@core/ui/codeitem-droplist/codeitem-droplist.component.ts
@@ -1,29 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {SelectItem} from 'primeng/primeng';
-interface City {
-  name: string;
-  code: string;
-}
+import {CodeitemService} from '../../data/system/codeitem.service';
 @Component({
   selector: 'ngx-ys-codeitem-droplist',
   templateUrl: './codeitem-droplist.component.html',
   styleUrls: ['./codeitem-droplist.component.scss'],
 })
 export class CodeitemDroplistComponent implements OnInit {
-  cities1: SelectItem[];
-  selectedCity1: City;
-  constructor() {
-    this.cities1 = [
-      {label: 'Select City', value: null},
-      {label: 'New York', value: {id: 1, name: 'New York', code: 'NY'}},
-      {label: 'Rome', value: {id: 2, name: 'Rome', code: 'RM'}},
-      {label: 'London', value: {id: 3, name: 'London', code: 'LDN'}},
-      {label: 'Istanbul', value: {id: 4, name: 'Istanbul', code: 'IST'}},
-      {label: 'Paris', value: {id: 5, name: 'Paris', code: 'PRS'}},
-    ];
+  items: SelectItem[] = [];
+  @Input() codeMap = '';
+  @Input() pleaseSelect = '请选择';
+  @Output() _selectedValue = new EventEmitter();
+  constructor(private codeitem: CodeitemService) {
   }
 
   ngOnInit() {
+    this.codeitem.convert(this.codeMap).then((res) => {
+      this.items.push({label: this.pleaseSelect, value: null});
+      for (let r in res) {
+        this.items.push({label: res[r], value: r});
+        // console.log(r + ' ' + res[r]);
+      }
+    });
   }
-
+  _onChange(event) {
+    this._selectedValue.emit(event.value);
+  }
 }
--- carexcn/trunk/web/src/app/@core/ui/ui.module.ts
+++ carexcn/trunk/web/src/app/@core/ui/ui.module.ts
@@ -8,13 +8,14 @@ import {CameraUploadComponent} from './camera/camera-upload.component';
 import {CalendarComponent} from './calendar/calendar.component';
 import {AutoinputComponent} from './autoinput/autoinput.component';
 import { CodeitemDroplistComponent } from './codeitem-droplist/codeitem-droplist.component';
+import { DroplistComponent } from './droplist/droplist.component';
 
 
 @NgModule({
   entryComponents: [],
-  exports: [UiTableModule, CameraComponent, CalendarComponent, AutoinputComponent, CodeitemDroplistComponent],
+  exports: [UiTableModule, CameraComponent, CalendarComponent, AutoinputComponent, CodeitemDroplistComponent, DroplistComponent],
   imports: [FormsModule, UiTableModule, RouterModule, FileUploadModule, CalendarModule, AutoCompleteModule, DropdownModule],
-  declarations: [CameraComponent, CameraUploadComponent, CalendarComponent, AutoinputComponent, CodeitemDroplistComponent],
+  declarations: [CameraComponent, CameraUploadComponent, CalendarComponent, AutoinputComponent, CodeitemDroplistComponent, DroplistComponent],
 })
 export class UiModule {
 }
