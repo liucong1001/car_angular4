169f516f999be0d67b51e2c61bfa6cd4817a18c2
chenlh <chenlh@34825b8f-b11c-4bce-a188-27078bf7e737>
2018-02-05 01:39:51 +0000

    动态图片表单根据证件类型独立完成整个动态表单的构建和显示适配

--- carexcn/trunk/web/src/app/@core/ui/bussiness/seller-info/seller-info.component.ts
+++ carexcn/trunk/web/src/app/@core/ui/bussiness/seller-info/seller-info.component.ts
@@ -1,15 +1,12 @@
-import {Component, Input, OnChanges, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {MerchantModel} from '../../../model/bussiness/merchant.model';
 import {Codeitem} from '../../../model/system/codeitem';
-import {ControlContainer, FormArray, FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
+import {ControlContainer, FormBuilder, FormGroup, NgForm} from '@angular/forms';
 import {IdcardService} from '../../../device/idcard.service';
 import {MessageService} from '../../../utils/message.service';
 import {CodeitemService} from '../../../data/system/codeitem.service';
 import {ErrorMessage} from '../../valid-error/valid-error.component';
 import {Marketphotomap} from '../../../model/system/market-photo-map';
-import {MarketService} from '../../../data/system/market.service';
-import {FileSystemService} from '../../../data/system/file-system.service';
-import {LocalstorageService} from '../../../cache/localstorage.service';
 
 @Component({
   selector: 'ngx-ys-seller-info',
@@ -94,8 +91,6 @@ export class SellerInfoComponent implements OnInit {
     private message: MessageService,
     private fb: FormBuilder,
     private _codeitem: CodeitemService,
-    private _market: MarketService,
-    private _localstorage: LocalstorageService,
   ) {
   }
 
@@ -116,46 +111,25 @@ export class SellerInfoComponent implements OnInit {
    * 页面初始化事件
    */
   ngOnInit() {
-    // 默认不用填写委托人
+    /**
+     * 默认不用填写委托人
+     */
     this.seller.controls.Trustee.disable();
     this.autoinput_cheshang_source_url += this.merchant.id + '/';
+    /**
+     * 设置好证件类型
+     */
     if (! this.certType) {
       this._codeitem.list('certType').then(res => this.certType = res as Codeitem[]);
     }
-    this.setCertificateConfig(this.seller.value.certType);
-  }
-  /**
-   * 需要构建出一个支持 赋值和读取的 photos form
-   * 能够 this._photosForm.values 拿到值，方便的构建成如下数据结构
-   * eg: photos:{'身份证正面':['1.jpg','2.jpg']}
-   * 更新时，从后台拿到数据值能直接赋值上去组件能够正常显示图片
-   * @type {FormGroup}
-   * @private
-   */
-  // photoType = '身份证';
-  addPhotoType() {
-    // (this.seller.get('_photos_') as FormArray).addControl(this.photoType, this.fb.array([]));
-  }
-  addPhoto() {
-    let photos = this.seller.get('_photos_') as FormArray;
-    // photos.push(this.fb.control());
-    // photos.push(this.fb.control('aa'));
-    // (this.seller.get('photos') as FormArray).push(this.fb.control('aaa'));
-    // let photo = this.seller.get(this.photoType) as FormArray;
-    // photo.push(this.fb.control('bb'));
-    // (this.seller.get(this.photoType) as FormArray).push(this.fb.control('bbb'));
-  }
-  viewPhotos(): void {
-    // console.info('图片表单', this.seller.controls._photos_);
-    // console.info('图片表单值', this.seller.value._photos_);
-  }
-  setCertificateConfig(certType) {
-    this._market.getCertificateConfig({
-      isApp: '0',
-      certificateCode: certType, // 证件类型代码集
-      business: '01', //  01 预审  02 过户
-      formName: '预审录入卖家', // 表单名称
-    } as Marketphotomap).then(res => this.initPhotoMap(res.json() as [Marketphotomap]));
+    /**
+     * 初始化照片动态表单
+     */
+    let sellerPhotos = this.seller.get('_photos_') as FormGroup;
+    if (sellerPhotos) {
+      this.seller.removeControl('_photos_');
+    }
+    this.seller.addControl('_photos_', this.fb.group({}));
   }
   /**
    * 初始化动态图片表单
@@ -163,94 +137,6 @@ export class SellerInfoComponent implements OnInit {
    */
   public photos_name = {}; // 动态图片表单要用的照片名称
   public photos_url = {}; // 图片局部放大指令要用的示例图片地址
-  initPhotoMap(marketphotomap_arr: [Marketphotomap]) {
-    let sellerPhotos = this.seller.get('_photos_') as FormGroup;
-    if (sellerPhotos) {
-      this.seller.removeControl('_photos_');
-    }
-    this.seller.addControl('_photos_', this.fb.group({}));
-    sellerPhotos = this.seller.get('_photos_') as FormGroup;
-    /**
-     * 需要处理的事情：
-     * 拿到 sort，name，min，max，photoType
-     * 需要处理的最终数据为：
-     * photos:{photoType:['1.jpg','2.jpg','3.jpg']}
-     */
-    let photo_name_tmp = {};
-    let photo_url_tmp = {};
-    marketphotomap_arr.forEach(r => {
-      photo_name_tmp[r.photoType] = r.name;
-      photo_url_tmp[r.photoType] = 'id:' + r.photoExample.fileId;
-      let i = 0;
-      while ( i < r.min) {
-        sellerPhotos.addControl(r.photoType, this.fb.array([
-          {
-            value: 'id:' + (r.photoExample ? r.photoExample.fileId : ''),
-            disabled: false,
-          }, // [Validators.required, Validators.maxLength(64)],
-        ]));
-        i++;
-      }
-      // /**
-      //  * 多张情况
-      //  */
-      // if ( r.max > r.min) {
-      //   let i = r.min;
-      //   while ( i < r.max) {
-      //     console.info('i--------', i);
-      //     // sellerPhotos.addControl(r.photoType + ' ' + i, this.fb.array(['id:' + (r.fileDescription ? r.fileDescription.id : '')]));
-      //     sellerPhotos.addControl(r.photoType, this.fb.array([
-      //       {
-      //         label: r.name + ' ' + i,
-      //         value: 'id:' + (r.fileDescription ? r.fileDescription.id : ''),
-      //         disabled: false,
-      //       }, // [Validators.required, Validators.maxLength(64)],
-      //     ]));
-      //     i++;
-      //   }
-      //   /**
-      //    * 单张情况
-      //    */
-      // } else if ( r.max === r.min) {
-      //   // sellerPhotos.addControl(r.photoType, this.fb.array(['id:' + (r.fileDescription ? r.fileDescription.id : '')]));
-      //   sellerPhotos.addControl(r.photoType, this.fb.array([
-      //     {
-      //       label: r.name,
-      //       value: 'id:' + (r.fileDescription ? r.fileDescription.id : ''),
-      //       disabled: false,
-      //     }, // [Validators.required, Validators.maxLength(64)],
-      //   ]));
-      //   /**
-      //    * 异常情况
-      //    */
-      // } else {
-      //   console.error('error: max < min.');
-      // }
-      // sellerPhotos.push(photoType);
-      // sellerPhotos.push(this.fb.control(r.photoType, this.fb.array([])));
-      // sellerPhotos.at(0).
-    });
-    this.photos_name = photo_name_tmp;
-    this.photos_url = photo_url_tmp;
-  }
-
-  /**
-   * 证件类型选择事件以切换要操作的图片清单
-   * @param event
-   */
-  certTypeSelecteFunc(certSelectValue) {
-    this.setCertificateConfig(certSelectValue);
-  }
-
-  /**
-   * 证件类型匹配函数
-   * @param {Codeitem} code1
-   * @param {Codeitem} code2
-   * @returns {boolean | boolean}
-   */
-  certificateTypeCompareWithFunc(code1: Codeitem, code2: Codeitem) {
-    return (code1 && code2) ? code1.code === code2.code : false;
-  }
 
   /**
    * 选择车商事件
@@ -259,16 +145,9 @@ export class SellerInfoComponent implements OnInit {
    */
   getSelectedCheshang(value) {
     value.certCode = value.filingPerson.certCode;
-    // console.info(value);
     this.seller.patchValue(value);
   }
 
-  /**
-   * 显示照片详情
-   */
-  showPhotoDetail() {
-    console.info('a');
-  }
   /**
    * 读取IC卡并将读取的信息赋值到卖家表单中
    */
