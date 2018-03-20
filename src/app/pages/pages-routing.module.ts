04023a80d056e336ee378a7239249b8354278d02
chenlh <chenlh@34825b8f-b11c-4bce-a188-27078bf7e737>
2018-03-20 08:57:44 +0000

    clean components, ui-features

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,8 +1,8 @@
-import { RouterModule, Routes } from '@angular/router';
-import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
 
-import { PagesComponent } from './pages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import {PagesComponent} from './pages.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 import {AuthGuardService} from '../@core/data/security/auth-guard.service';
 
 const routes: Routes = [{
@@ -29,66 +29,60 @@ const routes: Routes = [{
       path: 'bill',
       loadChildren: './bill/bill.module#BillModule',
       canActivate: [AuthGuardService],
-    },{
-    path: 'archives-manage',
-    canActivate: [AuthGuardService],
-    loadChildren: './archives-manage/archives-manage.module#ArchivesManageModule',
-  }, {
-    path: 'common-auction',
-    canActivate: [AuthGuardService],
-    loadChildren: './common-auction/common-auction.module#CommonAuctionModule',
-  }, {
-    path: 'query-count',
-    canActivate: [AuthGuardService],
-    loadChildren: './query-count/query-count.module#QueryCountModule',
-  }, {
+    }, {
+      path: 'archives-manage',
+      canActivate: [AuthGuardService],
+      loadChildren: './archives-manage/archives-manage.module#ArchivesManageModule',
+    }, {
+      path: 'common-auction',
+      canActivate: [AuthGuardService],
+      loadChildren: './common-auction/common-auction.module#CommonAuctionModule',
+    }, {
+      path: 'query-count',
+      canActivate: [AuthGuardService],
+      loadChildren: './query-count/query-count.module#QueryCountModule',
+    }, {
       path: 'trade-change',
       canActivate: [AuthGuardService],
       loadChildren: './trade-change/trade-change.module#TradeChangeModule',
-  }, {
-    path: 'system',
-    canActivate: [AuthGuardService],
-    loadChildren: './system/system.module#SystemModule',
-  },{
+    }, {
+      path: 'system',
+      canActivate: [AuthGuardService],
+      loadChildren: './system/system.module#SystemModule',
+    }, {
       path: 'reportForm',
       canActivate: [AuthGuardService],
       loadChildren: './report-form/report-form.module#ReportFormModule',
-    },{
-    path: 'manager',
-    canActivate: [AuthGuardService],
-    loadChildren: './manager/manager.module#ManagerModule',
-  }, {
-    path: 'merchant',
-    loadChildren: './merchant/merchant.module#MerchantModule',
-  }, {
-    path: 'ui-features',
-    loadChildren: './ui-features/ui-features.module#UiFeaturesModule',
-  }, {
-    path: 'components',
-    loadChildren: './components/components.module#ComponentsModule',
-  }, {
-    path: 'maps',
-    loadChildren: './maps/maps.module#MapsModule',
-  }, {
-    path: 'charts',
-    loadChildren: './charts/charts.module#ChartsModule',
-  }, {
-    path: 'editors',
-    loadChildren: './editors/editors.module#EditorsModule',
-  }, {
-    path: 'forms',
-    loadChildren: './forms/forms.module#FormsModule',
-  }, {
-    path: 'tables',
-    loadChildren: './tables/tables.module#TablesModule',
-  }, {
-    path: 'example',
-    loadChildren: './example/example.module#ExampleModule',
-  }, {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
-  }],
+    }, {
+      path: 'manager',
+      canActivate: [AuthGuardService],
+      loadChildren: './manager/manager.module#ManagerModule',
+    }, {
+      path: 'merchant',
+      loadChildren: './merchant/merchant.module#MerchantModule',
+    }, {
+      path: 'maps',
+      loadChildren: './maps/maps.module#MapsModule',
+    }, {
+      path: 'charts',
+      loadChildren: './charts/charts.module#ChartsModule',
+    }, {
+      path: 'editors',
+      loadChildren: './editors/editors.module#EditorsModule',
+    }, {
+      path: 'forms',
+      loadChildren: './forms/forms.module#FormsModule',
+    }, {
+      path: 'tables',
+      loadChildren: './tables/tables.module#TablesModule',
+    }, {
+      path: 'example',
+      loadChildren: './example/example.module#ExampleModule',
+    }, {
+      path: '',
+      redirectTo: 'dashboard',
+      pathMatch: 'full',
+    }],
 }];
 
 @NgModule({
