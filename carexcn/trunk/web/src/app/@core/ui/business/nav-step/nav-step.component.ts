5cab3e59a22968fcf846962a7ddb48871657875e
chenlh <chenlh@34825b8f-b11c-4bce-a188-27078bf7e737>
2018-03-21 02:38:30 +0000

    预审录入 步骤导航

--- carexcn/trunk/web/src/app/@core/ui/business/nav-step/nav-step.component.ts
+++ carexcn/trunk/web/src/app/@core/ui/business/nav-step/nav-step.component.ts
@@ -8,13 +8,42 @@ import {MenuItem} from 'primeng/primeng';
   encapsulation: ViewEncapsulation.None,
 })
 export class NavStepComponent implements OnInit {
-  @Input() items: Array<MenuItem>;
+  /**
+   * 导航菜单核心数据
+   * @type {number}
+   */
+  @Input() steps: MenuItem[];
+  /**
+   * 当前导航第几步
+   * @type {number}
+   */
+  @Input() activeIndex = 0;
+
   /**
    * 本组件提供可配置化的步骤导航功能
    * 1、明显清晰的步骤说明
    * 2、当前处于第几步的标识
    * 3、方便的页面跳转
    */
-  constructor() { }
-  ngOnInit() {}
+  constructor() {
+  }
+
+  ngOnInit() {
+    if (this.steps) {
+      for (let menu in this.steps) {
+        if (Number(menu) > this.activeIndex) {
+          this.steps[menu].disabled = true;
+          delete this.steps[menu].routerLink;
+        }
+      }
+    }
+  }
+
+  /**
+   * 导航跳转事件
+   */
+  activeIndexChangeEvent(event) {
+    this.activeIndex = event;
+    console.info('activeIndexChangeEvent', event);
+  }
 }
