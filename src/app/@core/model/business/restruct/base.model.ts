ef0e3274e9cebf9a629be702f69b912c2fcfda0c
chenlh <chenlh@34825b8f-b11c-4bce-a188-27078bf7e737>
2018-03-29 09:36:02 +0000

    交易数据结构重构

---
+++ src/app/@core/model/business/restruct/base.model.ts
@@ -0,0 +1,12 @@
+export class BaseModel_continue {
+  /**
+   * 当前用户
+   * [所有情况必须]
+   */
+  cloudUser: string;
+  createUserId: string;
+  createTime: string;
+  lastModifyTime: string;
+  lastModifyUserId: string;
+  id: string;
+}
