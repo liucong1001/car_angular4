e7bc40678a707eab9be30c66d5327ef35f7bb208
chenlh <chenlh@34825b8f-b11c-4bce-a188-27078bf7e737>
2018-03-29 09:36:02 +0000

    交易数据结构重构

---
+++ carexcn/trunk/web/src/app/@core/model/business/restruct/status.enum.ts
@@ -0,0 +1,9 @@
+enum StatusEnum {
+//   HasBeenEntered = '00',
+//   HasBeenReviewed = '01',
+//   HasBeenCompleted = '02',
+//   HasBeenDeleted = '03',
+//   toBeEntered = '04',
+//   IsEntering = '05',
+//   HasBeenReturned = '06',
+}
