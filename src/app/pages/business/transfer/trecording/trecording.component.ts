ec66f7125781697f535777605c2e4d5ad339a7a0
chenlh <chenlh@34825b8f-b11c-4bce-a188-27078bf7e737>
2018-04-08 05:44:07 +0000

    过户录入 60%

--- src/app/pages/business/transfer/trecording/trecording.component.ts
+++ src/app/pages/business/transfer/trecording/trecording.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {MessageService} from '../../../../@core/utils/message.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {TradeForm} from '../../../../@core/model/business/trade/trade.form';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {TradeService} from '../../../../@core/data/business/trade.service';
 import {LocalstorageService} from '../../../../@core/cache/localstorage.service';
 import {BusinessFormGroup} from '../../business.form-group';
+import {BusinessTradeForm} from '../../../../@core/model/business/restruct/business.trade.form';
 
 @Component({
   selector: 'ngx-ys-trecording',
@@ -13,10 +13,9 @@ import {BusinessFormGroup} from '../../business.form-group';
   styleUrls: ['./trecording.component.scss'],
 })
 export class TrecordingComponent implements OnInit {
-  private _cache_pre = 'business_transfer_recording_';
-  public archiveNo = '';
-  public trade: TradeForm;
-  public tradeList: [TradeForm];
+  public businessTradeForm: BusinessTradeForm = null;
+  public notNewCar = true;
+  public tradeList: [BusinessTradeForm];
   public _vehicleFormGroup: FormGroup = this._formBuilder.group({
     preVehicle: this._businessFormGroup.vehicleAndData,
     photos: this._formBuilder.group({}),
@@ -35,39 +34,50 @@ export class TrecordingComponent implements OnInit {
     private _localstorage: LocalstorageService,
   ) {}
   getTradeByArchiveNoComponent(trade) {
-    this.trade = trade;
-    this._sellerFormGroup.patchValue(this.trade.seller);
+    this.businessTradeForm = trade as BusinessTradeForm;
+    this._sellerFormGroup.patchValue(this.businessTradeForm.seller);
+    this._vehicleFormGroup.patchValue(this.businessTradeForm.preVehicle);
   }
   getTradeListByArchiveNoComponent(tradeList) {
     this.tradeList = tradeList;
     this.onChangeSelectedCar(tradeList[0]);
   }
   ngOnInit() {
-    let maybe_continue_archiveNo = this._localstorage.get(this._cache_pre + 'archiveNo');
-    if (maybe_continue_archiveNo) {
-      this.archiveNo = maybe_continue_archiveNo;
+    let trecording_trade_form = this._localstorage.get('business_trecording_trade_form') as BusinessTradeForm;
+    if (trecording_trade_form) {
+      this.businessTradeForm = trecording_trade_form;
+    } else {
+      let recording_trade_form = this._localstorage.get('business_recording_trade_form') as BusinessTradeForm;
+      if (recording_trade_form) {
+        this.businessTradeForm = {preVehicle: {preVehicle: {
+          merchant: recording_trade_form.preVehicle.preVehicle.merchant,
+        }}} as BusinessTradeForm;
+      }
     }
   }
-  onChangeSelectedCar(trade: TradeForm): void {
+  onChangeSelectedCar(trade: BusinessTradeForm): void {
     if (null === trade) {
       this._vehicleFormGroup.reset();
-      // this.notNewCar = false;
+      this.notNewCar = false;
       this._message.info('添加车辆', '添加新车辆');
     } else {
-      // this.notNewCar = true;
+      this.notNewCar = true;
       console.info(trade.preVehicle);
-      this._vehicleFormGroup.patchValue({vehicle: trade.preVehicle});
+      this._vehicleFormGroup.patchValue(trade.preVehicle);
       this._message.info('查看车辆', trade.preVehicle.preVehicle.plateNumber);
     }
   }
   onSubmit() {
     console.info(this.tradeList.length);
     console.info(this.tradeList);
-    this._localstorage.set(this._cache_pre + 'archiveNo', this.trade.archiveNo);
+    /**
+     * 过户录入，所有车辆
+     */
+    this._localstorage.set('business_trecording_trade_form', this.businessTradeForm);
     if (1 > this.tradeList.length) {
       this._message.warning('错误提示', '请选择至少一个车辆。');
     } else {
-      this._localstorage.set(this._cache_pre + 'trade', this.tradeList);
+      // this._localstorage.set(this._cache_pre + 'trade', this.tradeList);
       this._router.navigateByUrl('/pages/business/transfer/trecording2');
     }
   }
--- src/app/pages/business/transfer/trecording2/trecording2.component.ts
+++ src/app/pages/business/transfer/trecording2/trecording2.component.ts
@@ -12,6 +12,7 @@ import {TransferVehicleModel} from '../../../../@core/model/business/trade/trans
 import {BuyerModel} from '../../../../@core/model/business/trade/buyer.model';
 import {FileSystemService} from '../../../../@core/data/system/file-system.service';
 import {FilingInfoModel} from '../../../../@core/model/business/filing.info.model';
+import {BusinessTradeForm} from '../../../../@core/model/business/restruct/business.trade.form';
 
 @Component({
   selector: 'ngx-trecording2',
@@ -19,11 +20,7 @@ import {FilingInfoModel} from '../../../../@core/model/business/filing.info.mode
   styleUrls: ['./trecording2.component.scss'],
 })
 export class Trecording2Component implements OnInit {
-  private _cache_pre = 'business_transfer_recording_';
-  vehicleCertificateFormConfig: Marketphotomap;
-  sellerCertificateFormConfig: Marketphotomap;
-  buyerCertificateFormConfig: Marketphotomap;
-  vehicleTransferCertificateFormConfig: Marketphotomap;
+  public businessTradeForm: BusinessTradeForm = null;
   public archiveNo = '';
   public trade: TradeForm;
   // public trade: TradeForm = {preVehicle: {preVehicle: {filingInfo: {}, merchant: {}}}};
@@ -54,81 +51,42 @@ export class Trecording2Component implements OnInit {
   }
 
   ngOnInit() {
-    let maybe_continue_archiveNo = this._localstorage.get(this._cache_pre + 'archiveNo');
-    if (maybe_continue_archiveNo) {
-      this.archiveNo = maybe_continue_archiveNo;
-    }
-    /**
-     * 卖家证件类型表单配置
-     * @type {{}}
-     */
-    this.sellerCertificateFormConfig = {
-      certificateCode: '00', // 证件类型代码集
-      formName: '过户录入卖家', // 表单名称
-    } as Marketphotomap;
-    /**
-     * 买家证件类型表单配置
-     * @type {{}}
-     */
-    this.buyerCertificateFormConfig = {
-      certificateCode: '00', // 证件类型代码集
-      formName: '过户录入买家', // 表单名称
-    } as Marketphotomap;
-    this.vehicleTransferCertificateFormConfig = {
-      certificateCode: '00', // 证件类型代码集
-      formName: '过户录入车辆', // 表单名称
-    } as Marketphotomap;
-    /**
-     * 处理缓存数据
-     * @type {any | any}
-     */
-    let maybe_vehicleTransfer = this._localstorage.get(this._cache_pre + 'vehicleTransfer');
-    if (maybe_vehicleTransfer) {
-      let maybe_buyer = this._localstorage.get(this._cache_pre + 'buyer');
-      if (maybe_buyer) {
-        this._formGroup.patchValue({
-          buyer: maybe_buyer,
-          vehicleTransfer: maybe_vehicleTransfer,
-        });
-        this._localstorage.get(this._cache_pre + 'buyer_photos');
-      } else {
-        this._formGroup.patchValue({
-          vehicleTransfer: maybe_vehicleTransfer,
-        });
-      }
+    let trecording_trade_form = this._localstorage.get('business_trecording_trade_form') as BusinessTradeForm;
+    if (trecording_trade_form) {
+      this.businessTradeForm = trecording_trade_form;
     }
   }
   onSubmit() {
     /**
      * 处理缓存数据
      */
-    this._localstorage.set(this._cache_pre + 'buyer', this._formGroup.get('buyer').value);
-    this._localstorage.set(this._cache_pre + 'buyer_photos', this._formGroup.get('buyer').get('_photos_').value);
-    this._localstorage.set('dynamic_photos_buyer_info', this._cache_pre + 'buyer_photos');
-    this._localstorage.set(this._cache_pre + 'vehicleTransfer', this._formGroup.get('vehicleTransfer').value);
+    // this._localstorage.set(this._cache_pre + 'buyer', this._formGroup.get('buyer').value);
+    // this._localstorage.set(this._cache_pre + 'buyer_photos', this._formGroup.get('buyer').get('_photos_').value);
+    // this._localstorage.set('dynamic_photos_buyer_info', this._cache_pre + 'buyer_photos');
+    // this._localstorage.set(this._cache_pre + 'vehicleTransfer', this._formGroup.get('vehicleTransfer').value);
     /**
      * 发送提交请求
      */
-    let transferVehicle = this._formGroup.get('vehicleTransfer').value as TransferVehicleModel;
-    // transferVehicle.vehicleManagement = {};
-    delete transferVehicle.vehicleManagement;
-    transferVehicle.filingInfo = this.trade.preVehicle.preVehicle.filingInfo as FilingInfoModel;
-    this._transfer.create(
-      this.trade.archiveNo,
-      this._file.filterPhotosValue(this._formGroup.get('buyer').get('_photos_').value),
-      this._formGroup.get('buyer').value as BuyerModel,
-      {},
-      // this._formGroup.get('vehicleTransfer').value as TransferVehicleModel,
-      transferVehicle,
-      this.trade.preVehicle.preVehicle.id,
-    ).then(res => {
-      console.info('res', res);
-      this._localstorage.set(this._cache_pre + 'transfer_trade', res);
-      this._router.navigateByUrl('/pages/business/transfer/trecording-last');
-    }).catch(e => {
-      console.info('e', e);
-      this._message.error('错误', e.message);
-    });
+    // let transferVehicle = this._formGroup.get('vehicleTransfer').value as TransferVehicleModel;
+    // // transferVehicle.vehicleManagement = {};
+    // delete transferVehicle.vehicleManagement;
+    // transferVehicle.filingInfo = this.trade.preVehicle.preVehicle.filingInfo as FilingInfoModel;
+    // this._transfer.create(
+    //   this.trade.archiveNo,
+    //   this._file.filterPhotosValue(this._formGroup.get('buyer').get('_photos_').value),
+    //   this._formGroup.get('buyer').value as BuyerModel,
+    //   {},
+    //   // this._formGroup.get('vehicleTransfer').value as TransferVehicleModel,
+    //   transferVehicle,
+    //   this.trade.preVehicle.preVehicle.id,
+    // ).then(res => {
+    //   console.info('res', res);
+    //   // this._localstorage.set(this._cache_pre + 'transfer_trade', res);
+    //   this._router.navigateByUrl('/pages/business/transfer/trecording-last');
+    // }).catch(e => {
+    //   console.info('e', e);
+    //   this._message.error('错误', e.message);
+    // });
   }
 
 }
